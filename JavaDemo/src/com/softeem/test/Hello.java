ea1b49536f75cf73f0b1fb78eac8f44ca66d779e
mincai <2310956312@qq.com>
2024-08-19 20:51:48 +0800

    这是一个test

--- JavaDemo/src/com/softeem/test/Hello.java
+++ JavaDemo/src/com/softeem/test/Hello.java
@@ -3,5 +3,6 @@ package com.softeem.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World2!");
+        System.out.println("this is a test!");
     }
 }
